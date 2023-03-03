4ac9a6076f4708f9e90fc1041ad8ea0e8517691a
SolYX1349 <saul77023@gmail.com>
2023-03-02 22:23:14 -0600

    Se agrego un la implementacion del test en un ejercicio

--- src/main/java/ejercicios_aleatorios/separacion_de_cadenas/Main.java
+++ src/main/java/ejercicios_aleatorios/separacion_de_cadenas/Main.java
@@ -3,15 +3,8 @@ package ejercicios_aleatorios.separacion_de_cadenas;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static void main(String[] args) {
-        String cadena = "nombre;apellido;ciudad;edad;pais;calle;hijos;padres;casa;algoritmos;estudios;peso;estatura;promedio";
-        for (String element : splitString(cadena)) {
-            System.out.println(element);
-        }
-    }
-
-    private static String[] splitString(String arg) {
-        Pattern pattern = Pattern.compile(";");
-        return pattern.split(arg);
+    public static String[] splitString(String text, String regEx) {
+        Pattern pattern = Pattern.compile(regEx);
+        return pattern.split(text);
     }
 }
