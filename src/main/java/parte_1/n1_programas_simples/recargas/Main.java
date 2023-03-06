aad8fa968d9d99e1e89ad1af965be82e8a67120f
SolYX1349 <saul77023@gmail.com>
2023-03-06 16:17:21 -0600

    Se agregaron dos test de programas simples

--- src/main/java/parte_1/n1_programas_simples/recargas/Main.java
+++ src/main/java/parte_1/n1_programas_simples/recargas/Main.java
@@ -1,19 +1,45 @@
 package parte_1.n1_programas_simples.recargas;
+
 import java.util.Scanner;
+
 public class Main {
+    private Scanner scanner;
+
+    //Codigo somple del ejercicio
     public static void main(String[] args) {
+
         Scanner inputScanner = new Scanner(System.in);
-        int numeroOriginal , numeroVerificacion;
+        int numberUser, numberToVerify;
 
-                System.out.print("Numero: ");
-                numeroOriginal = inputScanner.nextInt();
-                System.out.print("verifica el numero");
-                numeroVerificacion = inputScanner.nextInt();
-                mensajeSystema(numeroOriginal,numeroVerificacion);
+        System.out.println("Number phone: ");
+        numberUser = inputScanner.nextInt();
 
+        System.out.println("Verify your number phone");
+        numberToVerify = inputScanner.nextInt();
+
+        if (numberUser == numberToVerify) {
+            System.out.println("Recharge done");
+        } else {
+            System.out.println("The number phone isn't correct");
+        }
     }
-    public static void mensajeSystema(int arg1 , int arg2){
-        if (arg1 == arg2){ System.out.println("Tu recarga fue echa con exito"); }
-        else { System.out.println("Hay un error porfavor verrifica "); }
+
+    //Codigo para test
+    public String run() {
+        String message = "";
+        int numberUser, numberToVerify;
+
+        System.out.println("Number phone: ");
+        numberUser = scanner.nextInt();
+
+        System.out.println("Verify your number phone");
+        numberToVerify = scanner.nextInt();
+
+        if (numberUser == numberToVerify) {
+            message = "Recharge done";
+        } else {
+            message = "The number phone isn't correct";
+        }
+        return message;
     }
 }
