652b071781aeabab6b30d9910e5330a46d7a1d0a
SolYX1349 <saul77023@gmail.com>
2023-03-06 17:30:32 -0600

    Se agrego el test de la carpeta de ejercicios ramdom

--- src/main/java/ejercicios_aleatorios/separacion_de_cadenas/Main.java
+++ src/main/java/ejercicios_aleatorios/separacion_de_cadenas/Main.java
@@ -3,8 +3,51 @@ package ejercicios_aleatorios.separacion_de_cadenas;
 import java.util.regex.Pattern;
 
 public class Main {
-    public static String[] splitString(String text, String regEx) {
-        Pattern pattern = Pattern.compile(regEx);
-        return pattern.split(text);
+    public String[] splitString(String text, String regEx) {
+        if (validationOfArguments(text, regEx)) {
+            Pattern pattern = Pattern.compile(regEx);
+            return pattern.split(text);
+        }
+        return new String[1];
+    }
+
+    public static boolean validationOfArguments(String txt, String regEx) {
+        boolean isValid = false;
+        if (validText(txt)){
+            if (valRegEx(regEx, txt)){
+                isValid = true;
+            }
+        }
+        return isValid;
+    }
+
+    public static boolean validText(String text) {
+        boolean isValid = true;
+        if (text == null) {
+            isValid = false;
+        } else {
+            if (text.isBlank()) {
+                isValid = false;
+            }
+            if (text.length() <= 1) {
+                isValid = false;
+            }
+        }
+        return isValid;
+    }
+
+    public static boolean valRegEx(String regEx, String text) {
+        boolean isValid = true;
+        if (regEx == null) {
+            isValid = false;
+        } else {
+            if (regEx.isBlank()) {
+                isValid = false;
+            }
+            if (!text.contains(regEx)) {
+                isValid = false;
+            }
+        }
+        return isValid;
     }
 }
