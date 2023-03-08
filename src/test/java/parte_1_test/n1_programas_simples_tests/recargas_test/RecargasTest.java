678d8c104d6b97994c2643d4c57ac2bfdb97a362
SolYX1349 <saul77023@gmail.com>
2023-03-07 22:02:28 -0600

    Se organizo la carpeta de parte 1 para una mejor organizacion

---
+++ src/test/java/parte_1_test/n1_programas_simples_tests/recargas_test/RecargasTest.java
@@ -0,0 +1,37 @@
+package parte_1_test.n1_programas_simples_tests.recargas_test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import parte_1.n1_programas_simples.recargas.Main;
+
+import java.util.Scanner;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RecargasTest {
+
+    @InjectMocks
+    Main main;
+
+    @Mock
+    Scanner scanner;
+
+    @Test
+    public void should_return_good_message() {
+        String messageExpected = "Recharge done";
+        when(scanner.nextInt()).thenReturn(123456789).thenReturn(123456789);
+        Assert.assertEquals(messageExpected, main.run());
+    }
+
+    @Test
+    public void should_return_bad_message() {
+        String message_expected = "The number phone isn't correct";
+        when(scanner.nextInt()).thenReturn(123456789).thenReturn(123456788);
+        Assert.assertEquals(message_expected, main.run());
+    }
+}
