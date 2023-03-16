2ba11637d59b56a2d6ec5481e9789c15c78bcad4
SolYX1349 <saul77023@gmail.com>
2023-03-15 19:48:54 -0600

    Se agrego un test de estructuras condicionales

--- src/main/java/parte_1/n2_estructuras_condicionales/anios_bisiestos/Main.java
+++ src/main/java/parte_1/n2_estructuras_condicionales/anios_bisiestos/Main.java
@@ -3,16 +3,26 @@ package parte_1.n2_estructuras_condicionales.anios_bisiestos;
 import javax.swing.*;
 
 public class Main {
+    //Codigo base del ejercicio
     public static void main(String[] args) {
 
-        String dialogoEntrada = JOptionPane.showInputDialog(null,"Digita un año:");
+        String dialogoEntrada = JOptionPane.showInputDialog(null, "Digita un año:");
 
         short anio = Short.parseShort(dialogoEntrada);
         byte anioBisiesto = (byte) (anio % 4);
-        if (anioBisiesto == 0){
-            JOptionPane.showMessageDialog(null,anio + " si es año bisiesto");
-        }else {
-            JOptionPane.showMessageDialog(null,anio + " no es año bisiesto");
+        if (anioBisiesto == 0) {
+            JOptionPane.showMessageDialog(null, anio + " si es año bisiesto");
+        } else {
+            JOptionPane.showMessageDialog(null, anio + " no es año bisiesto");
         }
+    }
+
+    //Codigo dirigido al testing
+    public static String isItLeapYear(Integer year) {
+        String outMessage = "Invalid Year";
+        if (year != null && year >= 0) {
+            outMessage = (year % 4) == 0 ? year + " It's Leap Year" : year + " Not a Leap Year";
+        }
+        return outMessage;
     }
 }
