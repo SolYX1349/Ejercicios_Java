03e7422cf9a3a0c559fcf234040d55248953a707
SolYX1349 <saul77023@gmail.com>
2023-04-06 00:32:01 -0600

    Se agregan archivos

--- src/main/java/ejercicios_aleatorios/desplazamiento_y_distancia/Solution.java
+++ src/main/java/ejercicios_aleatorios/desplazamiento_y_distancia/Solution.java
@@ -1,7 +1,14 @@
 package ejercicios_aleatorios.desplazamiento_y_distancia;
 
-public class Displacement {
-    public static int[] twoSum(int[] arrayNumbers, int expectedSum) {
+public class Solution {
+    public static void main(String[] args){
+        int[] arrayPosition = {2, 7, 11, 15};
+        for (int item : twoSum(arrayPosition, 9)) {
+            System.out.println(item);
+        }
+    }
+
+    public static int[] twoSum(int[] arrayNumbers, int expectedSum) throws Exception {
         int[] indexValid = new int[2];
         if (arrayNumbers.length >= 1) {
             for (int i = 0; i < arrayNumbers.length - 1; i++) {
--- src/test/java/ejercicios_aleatorios_test/the_number_is_palindrome_test/SolutionTest.java
+++ src/test/java/ejercicios_aleatorios_test/the_number_is_palindrome_test/SolutionTest.java
@@ -1,23 +1,23 @@
 package ejercicios_aleatorios_test.the_number_is_palindrome_test;
 
-import ejercicios_aleatorios.the_number_is_palindrome.PalindromeNumber;
+import ejercicios_aleatorios.the_number_is_palindrome.Solution;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class PalindromeNumberTest {
+public class SolutionTest {
 
     @Test
     public void should_return_false_when_the_number_be_negative() {
-        Assert.assertFalse(PalindromeNumber.isPalindrome(-123));
+        Assert.assertFalse(Solution.isPalindrome(-123));
     }
 
     @Test
     public void should_return_false_when_the_number_not_be_palindrome() {
-        Assert.assertFalse(PalindromeNumber.isPalindrome(123));
+        Assert.assertFalse(Solution.isPalindrome(123));
     }
 
     @Test
     public void should_return_true_when_the_number_be_palindrome() {
-        Assert.assertTrue(PalindromeNumber.isPalindrome(121));
+        Assert.assertTrue(Solution.isPalindrome(121));
     }
 }
